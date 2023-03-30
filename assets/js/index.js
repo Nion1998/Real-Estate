ea9b35b7861ab257eb35a63327a42c1fca6ffa6f
Nion1998 <55551279+Nion1998@users.noreply.github.com>
2023-03-30 16:50:06 +0600

    amimation

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,39 @@ const navbar = document.querySelector('.navbar');
                 navbar.classList.remove('navbar-scroll');
             }
         };
+
+        const priceRange = document.getElementById("price-range");
+        const priceValue = document.getElementById("price-value");
+        
+        priceRange.oninput = function() {
+          priceValue.innerHTML = "$" + this.value;
+        }
+        
+
+        const counter = document.querySelector('.count');
+        const rangeStart = 1;
+        const rangeEnd = 10;
+
+
+$(document).ready(function(){
+    $(".counter").each(function(){
+        $(this).prop('Counter',0).animate({
+            counter:$(this).text()
+        },{
+            duration:3500,
+            easing :'swing',
+            step:function(now){
+                $(this).text(Math.ceil(now))
+            }
+        })
+    });
+});
+
+gsap.fromTo( ".logo",{duration:1 , x: 1000 }, {duration:1 , x: 0 , });
+
+gsap.from(".nav-item",{duration:1 , opacity:0 , y: -150 ,stagger:0.25});
+
+gsap.from(".card",{duration:2.5 , opacity:0 , scale:0.3 });
+
+//Dot animatio use gsap 
+gsap.from(".dot",{duration:1 , opacity:0 , y: -150 ,stagger:0.25});
